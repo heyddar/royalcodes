fff64bcbb734256784592d77a62170065bbfea8a
heyddar <afshar13ali@gmail.com>
2020-07-28 17:33:29 +0430

    first commit

---
+++ app/Answer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+    public function Thread()
+    {
+        $this->belongsTo(Thread::class);
+    }
+}
