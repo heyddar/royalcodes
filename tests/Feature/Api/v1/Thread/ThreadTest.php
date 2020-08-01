a63c57089cdc82c2ebcbac7ced84d88bda71c6dc
heyddar <afshar13ali@gmail.com>
2020-08-02 04:08:51 +0430

    add threads#2

--- tests/Feature/Api/v1/Thread/ThreadTest.php
+++ tests/Feature/Api/v1/Thread/ThreadTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\Api\v1\Thread;
 
+use App\Channel;
 use App\Thread;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -26,6 +29,30 @@ class ThreadTest extends TestCase
         $response = $this->get(route('threads.show', [$thread->slug]));
 
         $response->assertStatus(Response::HTTP_OK);
+    }
+
+    /** @test */
+    public function thread_should_be_validated()
+    {
+
+        $response = $this->postJson(route('threads.store', []));
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function can_create_thread()
+    {
+
+        Sanctum::actingAs(factory(User::class)->create());
+
+        $response = $this->postJson(route('threads.store', [
+            'title'     => 'foo',
+            'content'   => 'bar',
+            'channel_id'=> factory(Channel::class)->create()->id,
+        ]));
+
+        $response->assertStatus(Response::HTTP_CREATED);
     }
 
 }
