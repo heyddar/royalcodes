4590e2a57f0c5f81004a9186f43872e38a87d611
heyddar <afshar13ali@gmail.com>
2020-08-30 20:28:33 +0430

    add subscribes#5

--- tests/Feature/Api/v1/Thread/SubscribeTest.php
+++ tests/Feature/Api/v1/Thread/SubscribeTest.php
@@ -3,10 +3,12 @@
 namespace Tests\Feature\Api\v1\Thread;
 
 use App\Channel;
+use App\Notifications\NewReplySubmited;
 use App\Thread;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
 use Laravel\Sanctum\Sanctum;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
@@ -48,5 +50,33 @@ class SubscribeTest extends TestCase
             'message' => 'user unsubscribed successfully'
         ]);
 
+    }
+
+    /**
+     * @test
+     */
+    public function notification_will_send_to_subscribers_of_a_thread()
+    {
+        $user = factory(User::class)->create();
+        Sanctum::actingAs($user);
+        Notification::fake();
+        $thread  = factory(Thread::class)->create();
+        $subscribe_response = $this->post(route('subscribe',[$thread]));
+
+        $subscribe_response->assertSuccessful();
+
+        $subscribe_response->assertJson([
+            'message' => 'user subscribed successfully'
+        ]);
+
+        $answer_response = $this->postJson(route('answers.store'),[
+            'content'   => 'foo',
+            'thread_id' => $thread->id
+        ]);
+        $answer_response->assertSuccessful();
+        $answer_response->assertJson([
+            'message' => 'answer submitted successfully'
+        ]);
+        Notification::assertSentTo($user, newReplySubmited::class);
     }
 }
